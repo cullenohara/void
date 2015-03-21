6d6a181e30b40090c64fd24dfa92371898fdc139
Cullen O'Hara <cullenohara@gmail.com>
2015-03-20 22:08:01 -0500

    yeah.. testing.

---
+++ Assets/scripts/GameTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTile : MonoBehaviour {
+
+	public int tileId;
+	public GameObject bombPrefab;
+
+	void Start ()
+	{
+		bombPrefab = GameObject.FindGameObjectWithTag("bomb");
+	}
+
+	void OnMouseEnter ()
+	{
+		print("poop.");
+		bombPrefab.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+	}
+
+}
