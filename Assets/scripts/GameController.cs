4f471065c18e17465ea015294f7fbf9a9b8edc49
Cullen O'Hara <cullenohara@gmail.com>
2015-03-22 00:09:00 -0500

    player movement.

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject ai;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void PlayerOneTurn ()
+	{
+		print ( "Player one's turn." );
+	}
+
+	void PlayerTwoTurn ()
+	{
+		print ( "Player two's turn." );
+	}
+}
--- Assets/scripts/GameTile.cs
+++ Assets/scripts/GameTile.cs
@@ -5,16 +5,66 @@ public class GameTile : MonoBehaviour {
 
 	public int tileId;
 	public GameObject bombPrefab;
+	public GameObject player;
+
+	public bool inUse = false;
+
+	public Vector3 clickPos;
+
+	public bool moveX = false;
+	public bool moveZ = false;
 
 	void Start ()
 	{
-		bombPrefab = GameObject.FindGameObjectWithTag("bomb");
+		bombPrefab = GameObject.FindGameObjectWithTag( "bomb" );
+		player = GameObject.FindGameObjectWithTag( "Player" );
 	}
 
-	void OnMouseEnter ()
+	void Update ()
 	{
-		print("poop.");
-		bombPrefab.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				if (hit.collider != null)
+				{
+					clickPos = hit.collider.transform.position;
+					moveX = true;
+				}
+			}
+		}
+
+		player.transform.position = Vector3.Lerp(player.transform.position, new Vector3(clickPos.x, player.transform.position.y, clickPos.z), Time.deltaTime * 3);
+
+	}
+
+	void GetPlayer (GameObject getPlayer)
+	{
+		player = getPlayer;
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			print ("Colliding");
+			inUse = true;
+		}
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			inUse = false;
+		}
+	}
+
+	void Move ()
+	{
+
 	}
 
 }
